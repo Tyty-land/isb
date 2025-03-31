7959385801a6e7a89bafcb22b35a4d39bded307a
Sasha_Pr()je/-1337 <ss-200909@mail.ru>
2025-03-31 18:07:07 +0400

    Создаю ветку

---
+++ lab_1_coder/coder_vizh.py
@@ -0,0 +1,88 @@
+def transformer(str_trans: str) -> str:
+    not_for_coder = [",", "-", ":", ";", ".", "!", "?", "–", "«", "»"]  # Можно дополнять
+    for i in not_for_coder:
+        if i in str_trans:
+            str_trans = str_trans.replace(i, "")
+    for j in range(0, len(str_trans)):
+        if ord(str_trans[j]) > 1071:
+            str_trans = str_trans[:j] + chr(ord(str_trans[j]) - 32) + str_trans[j + 1:]
+        if ord('A') <= ord(str_trans[j]) <= ord('Z'):
+            str_trans = str_trans[:j] + chr(ord(str_trans[j]) - ord('A') + 1040) + str_trans[j + 1:]
+        if ord('a') <= ord(str_trans[j]) <= ord('z'):
+            str_trans = str_trans[:j] + chr(ord(str_trans[j]) - 32 - ord('A') + 1040) + str_trans[j + 1:]
+
+    return str_trans
+
+
+def coder_vizhener(original_text: str, key_word: str, oper_mod: bool) -> str:
+    alphabet = [chr(1040 + x) for x in range(0, 32)]
+    alphabet.append(" ")
+
+    # Делаем ключ и исходный текст с заглавными буквами и без знаков препинания
+    original_text = transformer(original_text)
+    key_word = transformer(key_word)
+
+    if len(key_word) > int(len(original_text) / 2):
+        key_word = key_word[:int(len(original_text) / 2)]
+
+    writer_files(key_word, "key_word.txt")
+
+    # Создаём строку из ключевых слов для дальнейшего шифрования текста
+    key_string = ""
+    for i in range(0, int(len(original_text) / len(key_word) + 1)):
+        key_string += key_word
+    if len(key_string) > len(original_text):
+        key_string = key_string[:-(len(key_string) - len(original_text))]
+
+    # Шифруем/Дешифруем сообщение по Виженеру
+    coded_text = original_text
+    if oper_mod:
+        for i in range(0, len(key_string)):
+            final_index = alphabet.index(coded_text[i]) + alphabet.index(key_string[i]) + 1
+            if final_index > len(alphabet) - 1:
+                final_index = final_index - len(alphabet)
+            coded_text = coded_text[:i] + alphabet[final_index] + coded_text[i + 1:]
+    else:
+        for i in range(0, len(key_string)):
+            final_index = alphabet.index(coded_text[i]) - alphabet.index(key_string[i]) - 1
+            if final_index < 0:
+                final_index = final_index + len(alphabet)
+            coded_text = coded_text[:i] + alphabet[final_index] + coded_text[i + 1:]
+    return coded_text
+
+
+def writer_files(text_for_write: str, name_file: str) -> None:
+    if ".txt" not in name_file:
+        name_file = name_file + ".txt"
+
+    for i in range(0, len(text_for_write)):
+        if i % 100 == 0 and i != 0:
+            text_for_write = text_for_write[:i] + text_for_write[i] + "\n" + text_for_write[i + 1:]
+
+    with open(name_file, "w") as file:
+        file.write(text_for_write)
+        print(f"Файл {name_file} создан")
+
+
+def main() -> None:
+    oper_mod = int(input("Выберите режим работы(1 - шифруем, 0 - дешифруем)> "))
+    text_file = input("Введите путь/название файла с текстом: ")
+    if ".txt" not in text_file:
+        text_file += ".txt"
+    key_word = input("Введите ключ: ")
+
+    text_string = ""
+    with open(text_file, "r") as file:
+        text_string = file.read()
+        if '\n' in text_string:
+            text_string = text_string.replace("\n", "")
+
+    print(text_string)
+    name_res_file = "decoded_text"
+    if oper_mod:
+        name_res_file = "coded_text"
+    writer_files(coder_vizhener(text_string, key_word, oper_mod), name_res_file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab_1_decoder/decoder.py
@@ -0,0 +1,152 @@
+from typing import List
+from time import sleep
+
+def sort_frequencies(array: List[list]) -> List[list]:
+    x = 0
+    while x != len(array[1]) - 1:
+        if array[1][x] < array[1][x + 1]:
+            y = x + 1
+            while y != 0 and array[1][y - 1] < array[1][y]:
+                tmp_frq = array[1][y]
+                tmp_ltr = array[0][y]
+                array[1][y] = array[1][y - 1]
+                array[0][y] = array[0][y - 1]
+                array[1][y - 1] = tmp_frq
+                array[0][y - 1] = tmp_ltr
+                y -= 1
+                if y == 0 or array[1][y - 1] >= array[1][y + 1]:
+                    x = y
+        else:
+            x += 1
+    return array
+
+def create_frequen_alpha(text: str) -> List[list]:
+    alpha_freque = [[], []]
+    cnt_all_letter = 0
+    for i in range(0, len(text)):
+        cnt_all_letter += 1
+        if text[i] not in alpha_freque[0]:
+            alpha_freque[0].append(text[i])
+            alpha_freque[1].append(1)
+        else:
+            alpha_freque[1][alpha_freque[0].index(text[i])] += 1
+    alpha_freque[1] = [cnt_letter / cnt_all_letter for cnt_letter in alpha_freque[1]]
+    return sort_frequencies(alpha_freque)
+
+def replace_list(list_els: list, cur: str, new: str) -> list:
+    for i in range(len(list_els)):
+        if list_els[i] == cur:
+            list_els[i] = new
+    return list_els
+
+def alpha_replace(data: list, new_alpha: list) -> list:
+    text_list = list(data[0])
+    for i in range(len(text_list)):
+        text_list[i] = "\n"+text_list[i]+"\n"
+    for i in range(len(data[1][0])):
+        text_list = replace_list(text_list, "\n"+data[1][0][i]+"\n", "\t"+new_alpha[i]+"\t")
+        data[1][0][i] = new_alpha[i]
+    data[0] = "".join(text_list).replace("\t", "")
+
+    return data
+
+def custom_replace(data: list, cur: str, new: str) -> list:
+    for i in range(len(cur)):
+        if cur[i] in data[0]:
+            if new[i] in data[1][0] and new[i] in data[0]:
+                for j in range(len(data[0])):
+                    if data[0][j] == new[i]:
+                        data[0] = data[0][:j] + cur[i] + data[0][j + 1:]
+                    elif data[0][j] == cur[i]:
+                        data[0] = data[0][:j] + new[i] + data[0][j + 1:]
+                sp = 0
+                for j in range(len(data[1][0])):
+                    if data[1][0][j] == cur[i]:
+                        data[1][0][j] = new[i]
+                        sp += 1
+                    elif data[1][0][j] == new[i]:
+                        data[1][0][j] = cur[i]
+                        sp += 1
+                    if sp == 2:
+                        break
+            else:
+                data[0] = data[0].replace(cur[i], new[i])
+                data[1][0][data[1][0].index(cur[i])] = new[i]
+
+    return data
+
+def change_text(text: str) -> str:
+    for i in range(len(text)):
+        if i % 100 == 0 and i != 0:
+            text = text[:i] + text[i]+"\n" + text[i+1:]
+    return text
+
+def print_al_fr(alpha_freque: List[list], cnt_in_line: int) -> None:
+    for_print = ['\"' + alpha_freque[0][x] + '\" = ' + str(alpha_freque[1][x]) for x in range(len(alpha_freque[0]))]
+    for i in range(len(for_print)):
+        if (i+1) % cnt_in_line == 0 and i != 0:
+            for_print[i] = for_print[i] + "\n"
+    print(" ".join(for_print).replace("\n ", "\n"))
+
+def main() -> None:
+    coded_text_file = input("Введите имя/путь файла с зашифрованным сообщением: ")
+    if ".txt" not in coded_text_file:
+        coded_text_file += ".txt"
+    coded_text = ""
+    with open(coded_text_file, "r", encoding='utf-8') as file:
+        coded_text = file.read()
+        if "\n" in coded_text:
+            coded_text = coded_text.replace("\n", "")
+
+    alpha_freque_orig = [' ', 'О', 'И', 'Е', 'А', 'Н', 'Т', 'С', 'Р', 'В', 'М', 'Л'
+        , 'Д', 'Я', 'К', 'П', 'З', 'Ы', 'Ь', 'У', 'Ч', 'Ж', 'Г'
+        , 'Х', 'Ф', 'Й', 'Ю', 'Б', 'Ц', 'Ш', 'Щ', 'Э', 'Ъ']
+    data_list = [coded_text, create_frequen_alpha(coded_text)]
+
+    switch = ""
+    while switch != "0":
+        print("Текущий текст: ")
+        print("")
+        print(change_text(data_list[0]))
+        print("")
+        print("Текущий алфавит: ")
+        print("")
+        print_al_fr(data_list[1], 4)
+        print("_______________Меню_______________")
+        print("[f] - Применить частотную замену")
+        print("[r] - Заменить")
+        print("[s] - Сохранить текущий ключ/текст")
+        print("[k] - Использовать ключ")
+        print("[0] - Выход")
+        print("----------------------------------")
+        switch = input("> ")
+        print("")
+        if switch == 'f':
+            data_list = alpha_replace(data_list, alpha_freque_orig)
+        elif switch == 'r':
+            cur = input("Что заменяем: ")
+            new = input("На что заменяем: ")
+            data_list = custom_replace(data_list, cur, new)
+        elif switch == 's':
+            with open("key_alpha.txt", "w") as file:
+                file.write("".join(data_list[1][0]))
+            print("Ключ сохранён")
+            with open("text_by_key.txt", "w") as file:
+                file.write(change_text(data_list[0]))
+            print("Текст сохранён")
+            sleep(2)
+        elif switch == 'k':
+            key_file = input("Укажите путь до файла с ключом: ")
+            if ".txt" not in key_file:
+                key_file = key_file + ".txt"
+            key_alpha = []
+            with open(key_file, "r") as file:
+                key_alpha = list(file.read())
+            data_list = alpha_replace(data_list, key_alpha)
+            print(data_list[1][0])
+
+
+if __name__ == '__main__':
+    main()
+
+
